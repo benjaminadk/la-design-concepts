dbde47b56f6cd2fbfb85b8eabf9e0316ac8d63ee
benjaminadk <benjaminadk@gmail.com>
2020-05-15 08:31:53 -0700

    Add lowercase, pluralize to ladc ff coll thumb cmd

--- javascript/cli/ladc-ff.js
+++ javascript/cli/ladc-ff.js
@@ -23,42 +23,53 @@ const main = async () => {
       const oldPath = path.join(process.cwd(), file)
 
       let newFile
-      if(brand === 'jf') {
+      if (brand === "jf") {
         newFile = file.replace("-400x400", "").replace("-01", "")
-      } else if(brand === 'kravet') {
-        newFile = file
-        .slice(0, file.length - 4)
-        .replace(/\s/g, "")
-        .replace(/\./g, "!") + ".jpg"
-      } else if(brand === 'rl') {
-        let pieces = file.split(' ')
-        newFile = pieces[pieces.length-1].replace('jpeg','jpg')
-      } else if(brand === 'curated') {
-        newFile = file.toLowerCase().replace('_1_1','_1')
-      } else if(brand === 'scalamandre') {
-        newFile = file.replace('_',"")
-      } else if(brand === 'osborne-little') {
-        newFile = file.slice(0,8) + '.jpg'
-      } else if(brand === 'roomshot') {
-        newFile = file.replace('.jpg', '_room.jpg')
-      } else if(brand === 'roomshot-fix') {
-        newFile = file.replace('_detail',"")
-      } else if(brand === 'fabricut-furniture') {
-        newFile = file.toLowerCase().replace(' &', "").replace(/\s/g,"-")
-      } else if(brand === 'min') {
-        newFile = file.replace('-min.jpg', '.jpg')
-      } else if(brand === 'fb') {
+      } else if (brand === "kravet") {
+        newFile =
+          file
+            .slice(0, file.length - 4)
+            .replace(/\s/g, "")
+            .replace(/\./g, "!") + ".jpg"
+      } else if (brand === "rl") {
+        let pieces = file.split(" ")
+        newFile = pieces[pieces.length - 1].replace("jpeg", "jpg")
+      } else if (brand === "curated") {
+        newFile = file.toLowerCase().replace("_1_1", "_1")
+      } else if (brand === "scalamandre") {
+        newFile = file.replace("_", "")
+      } else if (brand === "osborne-little") {
+        newFile = file.slice(0, 8) + ".jpg"
+      } else if (brand === "roomshot") {
+        newFile = file.replace(".jpg", "_room.jpg")
+      } else if (brand === "roomshot-fix") {
+        newFile = file.replace("_detail", "")
+      } else if (brand === "fabricut-furniture") {
+        newFile = file.toLowerCase().replace(" &", "").replace(/\s/g, "-")
+      } else if (brand === "min") {
+        newFile = file.replace("-min.jpg", ".jpg")
+      } else if (brand === "fb") {
         newFile = "fb-" + file
-      } else if(brand === 'seabrook-room') {
-        newFile = file.replace('-A','_room').replace('-R','_room')
-      } else if(brand === 'thumb') {
-        newFile = file.replace('.jpg','_thumb.jpg')
-      } else if(brand === 'room-fix') {
-        newFile = file.replace('_room_room','_room')
-      } else if(brand === 'decorative-crafts') {
-        newFile = file.replace('-silo-xx','').replace('-xx','').replace('-silo-xx','').replace('-silo-for-web-xx','').replace('silo-xx','')
-      } else if(brand === 'cole-son') {
-        newFile = file.replace('_r1',"")
+      } else if (brand === "seabrook-room") {
+        newFile = file.replace("-A", "_room").replace("-R", "_room")
+      } else if (brand === "thumb") {
+        newFile = file.replace(".jpg", "_thumb.jpg")
+      } else if (brand === "room-fix") {
+        newFile = file.replace("_room_room", "_room")
+      } else if (brand === "decorative-crafts") {
+        newFile = file
+          .replace("-silo-xx", "")
+          .replace("-xx", "")
+          .replace("-silo-xx", "")
+          .replace("-silo-for-web-xx", "")
+          .replace("silo-xx", "")
+      } else if (brand === "cole-son") {
+        newFile = file.replace("_r1", "")
+      } else if (brand === "coll-thumb") {
+        newFile = file
+          .toLowerCase()
+          .replace("-400x400", "")
+          .replace("fabric", "fabrics")
       }
 
       const newPath = path.join(process.cwd(), newFile)
