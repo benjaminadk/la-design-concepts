f89f85bf3293b555d88c09ed8a00be13248e75d7
benjaminadk <benjaminadk@gmail.com>
2021-03-26 10:13:33 -0700

    update brand file

--- javascript/cli/ladc-samples.js
+++ javascript/cli/ladc-samples.js
@@ -27,298 +27,7 @@ program
 
   .parse(process.argv)
 
-const BRANDS = [
-  {
-    name: "Schumacher",
-    samples: [],
-    to: { name: " Morgan", email: "mshepard@fsco.com" },
-  },
-  {
-    name: "Kravet",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Brunschwig",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Lee Jofa",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "GP",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Winfield",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Andrew Martin",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Cole",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Threads",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Clarke",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Groundworks",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Baker Lifestyle",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Mulberry",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Gaston",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Laura Ashley",
-    samples: [],
-    to: { name: " Mary", email: "mary.scinta@kravet.com" },
-  },
-  {
-    name: "Galbraith",
-    samples: [],
-    to: { name: " Fyfe", email: "samples@thomaslavin.com" },
-  },
-  {
-    name: "Christopher Farr",
-    samples: [],
-    to: { name: " Fyfe", email: "samples@thomaslavin.com" },
-  },
-  {
-    name: "Opuzen",
-    samples: [],
-    to: { name: " Fyfe", email: "samples@thomaslavin.com" },
-  },
-  {
-    name: "Calvin",
-    samples: [],
-    to: { name: " Fyfe", email: "samples@thomaslavin.com" },
-  },
-  {
-    name: "Maya Romanoff",
-    samples: [],
-    to: { name: " Fyfe", email: "samples@thomaslavin.com" },
-  },
-  {
-    name: "Castel Maison",
-    samples: [],
-    to: { name: " Fyfe", email: "samples@thomaslavin.com" },
-  },
-  {
-    name: "Scalamandre",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Nicolette Mayer",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Old World Weavers",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Lelievre",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Christian Fischbacher",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Aldeco",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Alhambra",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Tassinari",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Grey Watkins",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Sandberg",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Jean Paul",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Colony",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Missoni Home",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Boris Kroll",
-    samples: [],
-    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
-  },
-  {
-    name: "Peter Dunham",
-    samples: [],
-    to: { name: " Sean", email: "sean@peterdunhamtextiles.com" },
-  },
-  {
-    name: "Ralph Lauren",
-    samples: [],
-    to: { name: "", email: "CustomerService@folia-fabrics.com" },
-  },
-  {
-    name: "Robert Allen",
-    samples: [],
-    to: { name: "", email: "orderscs@tradgroup.com" },
-  },
-  {
-    name: "Suburban Home",
-    samples: [],
-    to: { name: "", email: "orderscs@tradgroup.com" },
-  },
-  {
-    name: "Duralee",
-    samples: [],
-    to: { name: "", email: "orderscs@tradgroup.com" },
-  },
-  {
-    name: "Highland Court",
-    samples: [],
-    to: { name: "", email: "orderscs@tradgroup.com" },
-  },
-  {
-    name: "Beacon Hill",
-    samples: [],
-    to: { name: "", email: "orderscs@tradgroup.com" },
-  },
-  {
-    name: "Bailey",
-    samples: [],
-    to: { name: "", email: "orderscs@tradgroup.com" },
-  },
-  {
-    name: "Jasper",
-    samples: [],
-    to: {
-      name: "Katia",
-      email: "samples@jaspershowroom.com",
-    },
-  },
-  {
-    name: "Templeton",
-    samples: [],
-    to: {
-      name: "Katia",
-      email: "samples@jaspershowroom.com",
-    },
-  },
-  {
-    name: "Seabrook",
-    samples: [],
-    to: {
-      name: "",
-      email: "samples@wallquest.com",
-    },
-  },
-  {
-    name: "AST",
-    samples: [],
-    to: { name: "", email: "samples@designalliancela.com" },
-  },
-  {
-    name: "Wallcoveting",
-    samples: [],
-    to: { name: "Brenda", email: "brenda@brendahouston.com" },
-  },
-  {
-    name: "Maxwell",
-    samples: [],
-    to: { name: "", email: "memo@maxwellfabrics.com" },
-  },
-  {
-    name: "Telefina",
-    samples: [],
-    to: { name: "", email: "memo@maxwellfabrics.com" },
-  },
-  {
-    name: "Kasmir",
-    samples: [],
-    to: { name: "", email: "service@kasmirfabrics.com" },
-  },
-  {
-    name: "Charlotte",
-    samples: [],
-    to: { name: "", email: "hello@charlottefabrics.com" },
-  },
-  {
-    name: "JF",
-    samples: [],
-    to: { name: "", email: "customerservice@jffabrics.com" },
-  },
-  {
-    name: "Brentano",
-    samples: [],
-    to: { name: "", email: "samples@Brentanofabrics.com" },
-  },
-  {
-    name: "Pierre Frey",
-    samples: [],
-    to: { name: "", email: "bronte.valyi@pierrefrey.com" },
-  },
-  {
-    name: "Thibaut",
-    samples: [],
-    to: { name: "Julie", email: "julie@thestudioatdrc.com" },
-  },
-]
-
+const BRANDS = require("./lib/samples-brands")
 const THOMAS_LAVIN = [
   "Galbraith",
   "Christopher Farr",
@@ -370,6 +79,13 @@ async function main() {
         ) {
           BRANDS.find((el) => el.name === "Thibaut")["samples"].push(
             `Thibaut ${item.meta_data[0]["display_value"]}`
+          )
+        } else if (
+          item.meta_data[0].hasOwnProperty("key") &&
+          item.meta_data[0]["key"].startsWith("Robert Allen")
+        ) {
+          BRANDS.find((el) => el.name === "Robert Allen")["samples"].push(
+            `Robert Allen ${item.meta_data[0]["display_value"]}`
           )
         } else {
           let name = item.meta_data.find((el) => el.key === "Name")["value"]
@@ -526,6 +242,7 @@ async function main() {
     var f12 = BRANDS.find((el) => el.name === "Mulberry")["samples"]
     var f13 = BRANDS.find((el) => el.name === "Gaston")["samples"]
     var f14 = BRANDS.find((el) => el.name === "Laura Ashley")["samples"]
+    var f15 = BRANDS.find((el) => el.name === "Parkertex")["samples"]
 
     BRANDS.find((el) => el.name === "Kravet")["samples"] = [
       ...f1,
@@ -542,6 +259,7 @@ async function main() {
       ...f12,
       ...f13,
       ...f14,
+      ...f15,
     ]
 
     BRANDS.find((el) => el.name === "Brunschwig")["samples"] = []
@@ -557,6 +275,27 @@ async function main() {
     BRANDS.find((el) => el.name === "Mulberry")["samples"] = []
     BRANDS.find((el) => el.name === "Gaston")["samples"] = []
     BRANDS.find((el) => el.name === "Laura Ashley")["samples"] = []
+    BRANDS.find((el) => el.name === "Parkertex")["samples"] = []
+
+    // Consolidate Osborne & Little Brands
+    var g1 = BRANDS.find((el) => el.name === "Osborne")["samples"]
+    var g2 = BRANDS.find((el) => el.name === "Designer")["samples"]
+    var g3 = BRANDS.find((el) => el.name === "Nina Campbell")["samples"]
+    var g4 = BRANDS.find((el) => el.name === "Matthew")["samples"]
+    var g5 = BRANDS.find((el) => el.name === "Lorca")["samples"]
+
+    BRANDS.find((el) => el.name === "Osborne")["samples"] = [...g1, ...g2, ...g3, ...g4, ...g5]
+    BRANDS.find((el) => el.name === "Designer")["samples"] = []
+    BRANDS.find((el) => el.name === "Nina Campbell")["samples"] = []
+    BRANDS.find((el) => el.name === "Matthew")["samples"] = []
+    BRANDS.find((el) => el.name === "Lorca")["samples"] = []
+
+     // Consolidate Wallcoveting
+     var h1 = BRANDS.find((el) => el.name === "Wallcoveting")["samples"]
+     var h2 = BRANDS.find((el) => el.name === "Brenda Houston")["samples"]
+
+     BRANDS.find((el) => el.name === "Wallcoveting")["samples"] = [...h1, ...h2]
+     BRANDS.find((el) => el.name === "Brenda Houston")["samples"] = []
 
     for (let brand of BRANDS) {
       if (brand.samples.length) {
@@ -579,6 +318,10 @@ async function main() {
             ? `234728 - Sample Request - ${order} Client - ${last_name}`
             : brand.name === "JF"
             ? `Account U36971 - Sample Request - ${order} Client - ${last_name}`
+            : brand.name === "Osborne"
+            ? `L.A. Design Concepts - Sample Request - ${order} Client - ${last_name}`
+            : brand.name === "Victoria"
+            ? `000987 - Sample Request - ${order} Client - ${last_name}`
             : `Sample Request - ${order} Client - ${last_name}`
 
         var console_brand = THOMAS_LAVIN.includes(brand.name) ? "Thomas Lavin" : brand.name
---
+++ javascript/cli/lib/samples-brands.js
@@ -0,0 +1,347 @@
+module.exports = [
+  {
+    name: "Schumacher",
+    samples: [],
+    to: { name: " Morgan", email: "mshepard@fsco.com" },
+  },
+  {
+    name: "Kravet",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Brunschwig",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Lee Jofa",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "GP",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Winfield",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Andrew Martin",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Cole",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Threads",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Clarke",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Groundworks",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Baker Lifestyle",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Mulberry",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Gaston",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Laura Ashley",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Parkertex",
+    samples: [],
+    to: { name: " Mary", email: "mary.scinta@kravet.com" },
+  },
+  {
+    name: "Galbraith",
+    samples: [],
+    to: { name: " Fyfe", email: "samples@thomaslavin.com" },
+  },
+  {
+    name: "Christopher Farr",
+    samples: [],
+    to: { name: " Fyfe", email: "samples@thomaslavin.com" },
+  },
+  {
+    name: "Opuzen",
+    samples: [],
+    to: { name: " Fyfe", email: "samples@thomaslavin.com" },
+  },
+  {
+    name: "Calvin",
+    samples: [],
+    to: { name: " Fyfe", email: "samples@thomaslavin.com" },
+  },
+  {
+    name: "Maya Romanoff",
+    samples: [],
+    to: { name: " Fyfe", email: "samples@thomaslavin.com" },
+  },
+  {
+    name: "Castel Maison",
+    samples: [],
+    to: { name: " Fyfe", email: "samples@thomaslavin.com" },
+  },
+  {
+    name: "Scalamandre",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Nicolette Mayer",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Old World Weavers",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Lelievre",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Christian Fischbacher",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Aldeco",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Alhambra",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Tassinari",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Grey Watkins",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Sandberg",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Jean Paul",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Colony",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Missoni Home",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Boris Kroll",
+    samples: [],
+    to: { name: " Becky", email: "bmcreynolds@scalamandre.com" },
+  },
+  {
+    name: "Peter Dunham",
+    samples: [],
+    to: { name: " Sean", email: "sean@peterdunhamtextiles.com" },
+  },
+  {
+    name: "Ralph Lauren",
+    samples: [],
+    to: { name: "", email: "CustomerService@folia-fabrics.com" },
+  },
+  {
+    name: "Robert Allen",
+    samples: [],
+    to: { name: "", email: "orderscs@tradgroup.com" },
+  },
+  {
+    name: "Suburban Home",
+    samples: [],
+    to: { name: "", email: "orderscs@tradgroup.com" },
+  },
+  {
+    name: "Duralee",
+    samples: [],
+    to: { name: "", email: "orderscs@tradgroup.com" },
+  },
+  {
+    name: "Highland Court",
+    samples: [],
+    to: { name: "", email: "orderscs@tradgroup.com" },
+  },
+  {
+    name: "Beacon Hill",
+    samples: [],
+    to: { name: "", email: "orderscs@tradgroup.com" },
+  },
+  {
+    name: "Bailey",
+    samples: [],
+    to: { name: "", email: "orderscs@tradgroup.com" },
+  },
+  {
+    name: "Jasper",
+    samples: [],
+    to: {
+      name: "Katia",
+      email: "samples@jaspershowroom.com",
+    },
+  },
+  {
+    name: "Templeton",
+    samples: [],
+    to: {
+      name: "Katia",
+      email: "samples@jaspershowroom.com",
+    },
+  },
+  {
+    name: "Seabrook",
+    samples: [],
+    to: {
+      name: "",
+      email: "samples@wallquest.com",
+    },
+  },
+  {
+    name: "Osborne",
+    samples: [],
+    to: {
+      name: "Billy",
+      email: "hudsonb@oalusa.com",
+    },
+  },
+  {
+    name: "Designer",
+    samples: [],
+    to: {
+      name: "Billy",
+      email: "hudsonb@oalusa.com",
+    },
+  },
+  {
+    name: "Nina Campbell",
+    samples: [],
+    to: {
+      name: "Billy",
+      email: "hudsonb@oalusa.com",
+    },
+  },
+  {
+    name: "Matthew",
+    samples: [],
+    to: {
+      name: "Billy",
+      email: "hudsonb@oalusa.com",
+    },
+  },
+  {
+    name: "Lorca",
+    samples: [],
+    to: {
+      name: "Billy",
+      email: "hudsonb@oalusa.com",
+    },
+  },
+  {
+    name: "AST",
+    samples: [],
+    to: { name: "", email: "samples@designalliancela.com" },
+  },
+  {
+    name: "Wallcoveting",
+    samples: [],
+    to: { name: "Brenda", email: "brenda@brendahouston.com" },
+  },
+  {
+    name: "Brenda Houston",
+    samples: [],
+    to: { name: "Brenda", email: "brenda@brendahouston.com" },
+  },
+  {
+    name: "Maxwell",
+    samples: [],
+    to: { name: "", email: "memo@maxwellfabrics.com" },
+  },
+  {
+    name: "Telefina",
+    samples: [],
+    to: { name: "", email: "memo@maxwellfabrics.com" },
+  },
+  {
+    name: "Kasmir",
+    samples: [],
+    to: { name: "", email: "service@kasmirfabrics.com" },
+  },
+  {
+    name: "Charlotte",
+    samples: [],
+    to: { name: "", email: "hello@charlottefabrics.com" },
+  },
+  {
+    name: "JF",
+    samples: [],
+    to: { name: "", email: "customerservice@jffabrics.com" },
+  },
+  {
+    name: "Brentano",
+    samples: [],
+    to: { name: "", email: "samples@Brentanofabrics.com" },
+  },
+  {
+    name: "Victoria",
+    samples: [],
+    to: { name: "", email: "fabric@victoriahagan.com" },
+  },
+  {
+    name: "Pierre Frey",
+    samples: [],
+    to: { name: "", email: "bronte.valyi@pierrefrey.com" },
+  },
+  {
+    name: "Thibaut",
+    samples: [],
+    to: { name: "Julie", email: "julie@thestudioatdrc.com" },
+  },
+]
+
