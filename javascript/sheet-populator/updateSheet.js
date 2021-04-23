eb148aca334fece7990d1c66823056c502d43799
benjaminadk <benjaminadk@gmail.com>
2021-04-23 15:52:07 -0700

    update google sheet populator

--- javascript/sheet-populator/updateSheet.js
+++ javascript/sheet-populator/updateSheet.js
@@ -1,4 +1,5 @@
 const { google } = require('googleapis')
+const moment = require('moment')
 const WooCommerceRestApi = require('@woocommerce/woocommerce-rest-api').default
 const { promisify } = require('util')
 
@@ -17,6 +18,9 @@ module.exports = async (auth) => {
     const updateSheet = promisify(sheets.spreadsheets.values.update).bind(
       sheets
     )
+    const batchUpdateSheet = promisify(sheets.spreadsheets.batchUpdate).bind(
+      sheets
+    )
 
     const res1 = await getSheet({
       spreadsheetId,
@@ -24,14 +28,16 @@ module.exports = async (auth) => {
     })
 
     const rows = res1.data.values
-    const lastOrder = rows ? rows[rows.length - 1][0] : process.env.LAST_ORDER
+    const lastOrder = rows ? rows[0][0] : process.env.LAST_ORDER
+    const rowIndex = rows ? rows.length + 2 : 2
 
     const res2 = await API.get(`orders/${lastOrder}`)
     const after = res2.data.date_created
 
     const res3 = await API.get(`orders`, {
       per_page: 100,
-      order: 'asc',
+      order: 'desc',
+      status: ['pending', 'processing', 'on-hold', 'completed'],
       after,
     })
 
@@ -52,16 +58,20 @@ module.exports = async (auth) => {
           var brandName
 
           try {
-            brandName = res4.data.attributes.find((el) => el.name === 'brand')[
-              'options'
-            ][0]
+            brandName = res4.data.attributes
+              .find((el) => el.name === 'brand')
+              ['options'][0].replace('&amp;', 'and')
           } catch (error) {
             custom = true
 
             try {
-              brandName = item.meta_data[0]['display_key']
+              brandName = item.meta_data[0]['display_key'].replace(
+                '&amp;',
+                'and'
+              )
             } catch (error) {
-              brandName = 'CUSTOM'
+              console.log(`Problem with order #${order.id}`)
+              keep = false
             }
           }
 
@@ -70,15 +80,66 @@ module.exports = async (auth) => {
       }
 
       if (keep) {
-        toProcess.push([order.id, order.date_created, brand, order.total])
+        toProcess.push([
+          order.id,
+          moment(order.date_created).format('M/D/YYYY'),
+          brand,
+          order.total,
+        ])
       }
     }
 
     const res5 = await updateSheet({
       spreadsheetId,
-      range: 'April!A2:D1000',
+      range: `April!A${rowIndex}:D1000`,
       valueInputOption: 'RAW',
       resource: { values: toProcess },
+    })
+
+    const res6 = await batchUpdateSheet({
+      spreadsheetId,
+      resource: {
+        requests: [
+          {
+            sortRange: {
+              range: {
+                sheetId: 0,
+                startRowIndex: 1,
+                endRowIndex: 1000,
+                startColumnIndex: 0,
+                endColumnIndex: 6,
+              },
+              sortSpecs: [
+                {
+                  sortOrder: 'DESCENDING',
+                  dataSourceColumnReference: {
+                    name: 'Order Number',
+                  },
+                },
+              ],
+            },
+          },
+          {
+            deleteDuplicates: {
+              range: {
+                sheetId: 0,
+                startRowIndex: 1,
+                endRowIndex: rows.length + res5.data.updatedRows + 1,
+                startColumnIndex: 0,
+                endColumnIndex: 6,
+              },
+              comparisonColumns: [
+                {
+                  sheetId: 0,
+                  dimension: 'COLUMNS',
+                  startIndex: 0,
+                  endIndex: 6,
+                },
+              ],
+            },
+          },
+        ],
+      },
     })
   } catch (error) {
     console.log(error)
