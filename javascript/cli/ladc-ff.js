f33aaece593aa4ab75a06a242c8059e3da2a9559
Benjamin <28043421+benjaminadk@users.noreply.github.com>
2021-09-13 20:08:33 -0700

    add osborne hash to format filename

--- javascript/cli/ladc-ff.js
+++ javascript/cli/ladc-ff.js
@@ -1,87 +1,88 @@
 #!/usr/bin/env node
 
-const program = require("commander")
+const program = require("commander");
 
-const path = require("path")
-const { readdir, rename } = require("./lib/utils")
+const path = require("path");
+const { readdir, rename } = require("./lib/utils");
 
 program
   .option("-b, --brand [name]", "Type of filename formatting based on brand", "kravet")
-  .parse(process.argv)
+  .parse(process.argv);
 
 const main = async () => {
-  const { brand } = program
-  console.log(brand)
+  const { brand } = program;
   try {
-    const files = await readdir(process.cwd())
+    const files = await readdir(process.cwd());
 
     for (let file of files) {
-      const oldPath = path.join(process.cwd(), file)
+      const oldPath = path.join(process.cwd(), file);
 
-      let newFile
+      let newFile;
       if (brand === "jf") {
-        newFile = file.replace("-400x400", "").replace("-01", "")
+        newFile = file.replace("-400x400", "").replace("-01", "");
       } else if (brand === "kravet") {
         newFile =
           file
             .slice(0, file.length - 4)
             .replace(/\s/g, "")
-            .replace(/\./g, "!") + ".jpg"
+            .replace(/\./g, "!") + ".jpg";
       } else if (brand === "rl") {
-        let pieces = file.split(" ")
-        newFile = pieces[pieces.length - 1].replace("jpeg", "jpg")
+        let pieces = file.split(" ");
+        newFile = pieces[pieces.length - 1].replace("jpeg", "jpg");
       } else if (brand === "curated") {
-        newFile = file.toLowerCase().replace("_1_1", "_1")
+        newFile = file.toLowerCase().replace("_1_1", "_1");
       } else if (brand === "scalamandre") {
-        newFile = file.replace("_", "")
+        newFile = file.replace("_", "");
       } else if (brand === "osborne-little") {
-        newFile = file.slice(0, 8) + ".jpg"
+        newFile = file.slice(0, 8) + ".jpg";
+      } else if (brand === "osborne-hash") {
+        newFile = file.replace(/_(.+)\.jpg/, ".jpg");
       } else if (brand === "roomshot") {
-        newFile = file.replace(".jpg", "_room.jpg")
+        newFile = file.replace(".jpg", "_room.jpg");
       } else if (brand === "roomshot-fix") {
-        newFile = file.replace("_detail", "")
+        newFile = file.replace("_detail", "");
       } else if (brand === "fabricut-furniture") {
-        newFile = file.toLowerCase().replace(" &", "").replace(/\s/g, "-")
+        newFile = file.toLowerCase().replace(" &", "").replace(/\s/g, "-");
       } else if (brand === "min") {
-        newFile = file.replace("-min.jpg", ".jpg")
+        newFile = file.replace("-min.jpg", ".jpg");
       } else if (brand === "fb") {
-        newFile = "fb-" + file
+        newFile = "fb-" + file;
       } else if (brand === "seabrook-room") {
-        newFile = file.replace("-A", "_room").replace("-R", "_room")
+        newFile = file.replace("-A", "_room").replace("-R", "_room");
       } else if (brand === "thumb") {
-        newFile = file.replace(".jpg", "_thumb.jpg")
+        newFile = file.replace(".jpg", "_thumb.jpg");
       } else if (brand === "room-fix") {
-        newFile = file.replace("_room_room", "_room")
+        newFile = file.replace("_room_room", "_room");
       } else if (brand === "decorative-crafts") {
         newFile = file
           .replace("-silo-xx", "")
           .replace("-xx", "")
           .replace("-silo-xx", "")
           .replace("-silo-for-web-xx", "")
-          .replace("silo-xx", "")
+          .replace("silo-xx", "");
       } else if (brand === "cole-son") {
-        newFile = file.replace("_r1", "")
+        newFile = file.replace("_r1", "");
       } else if (brand === "coll-thumb") {
-        newFile = file.toLowerCase().replace("-400x400", "").replace("fabric", "fabrics")
+        newFile = file.toLowerCase().replace("-400x400", "").replace("fabric", "fabrics");
       } else if (brand === "JPG") {
-        newFile = file.replace(".JPG", ".jpg")
+        newFile = file.replace(".JPG", ".jpg");
       } else if (brand === "tyler") {
-        newFile = file.replace("400x400-", "")
+        newFile = file.replace("400x400-", "");
       } else if (brand === "sch-image") {
-        newFile = file.replace("---", "-").replace("'", "").replace("--", "-")
+        newFile = file.replace("---", "-").replace("'", "").replace("--", "-");
       } else if (brand === "ralph") {
-        newFile = file.toLowerCase().replace(";", "").replace(/\s/g, "-").replace(".jpeg", ".jpg")
+        newFile = file.toLowerCase().replace(";", "").replace(/\s/g, "-").replace(".jpeg", ".jpg");
       } else if (brand === "lower") {
-        newFile = file.toLowerCase()
+        newFile = file.toLowerCase();
       } else if (brand === "kasmir") {
-        newFile = file.replace(/\s/g, "-")
+        newFile = file.replace(/\s/g, "-");
       } else if (brand === "maya") {
-        newFile = file.replace("-swatch-", "-")
+        newFile = file.replace("-swatch-", "-");
       } else if (brand === "room-case") {
-        newFile = file.replace("_Room", "_room")
+        newFile = file.replace("_Room", "_room");
       } else if (brand === "gal") {
-        newFile = file.replace("-300x300", "")
-        console.log(newFile)
+        newFile = file.replace("-300x300", "");
+        console.log(newFile);
       } else if (brand === "gal2") {
         newFile = file
           .toLowerCase()
@@ -105,22 +106,22 @@ const main = async () => {
           .replace("_33.75x18", "")
           .replace("notrim", "")
           .replace("-36_125x18", "")
-          .replace("_", "-")
+          .replace("_", "-");
       } else if (brand === "gal3") {
-        newFile = file.replace(".jpg", "_room.jpg")
+        newFile = file.replace(".jpg", "_room.jpg");
       } else if (brand === "bella1") {
-        newFile = file.replace("_detail", "")
+        newFile = file.replace("_detail", "");
       } else if (brand === "bella2") {
-        newFile = file.replace("_detail", "_thumb")
+        newFile = file.replace("_detail", "_thumb");
       }
 
-      const newPath = path.join(process.cwd(), newFile)
+      const newPath = path.join(process.cwd(), newFile);
 
-      await rename(oldPath, newPath)
+      await rename(oldPath, newPath);
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
-main()
+main();
